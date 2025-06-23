68979952f28804a0170e6bac333fe1cd3b99304a
dfcut8 <dfcut8@proton.me>
2025-06-23 10:23:36 -0400

    it moves now

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -3,10 +3,17 @@ using UnityEngine.InputSystem;
 
 public class PlayerManager : MonoBehaviour
 {
+    public float speed = 1f;
     Vector2 moveInput;
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 delta = moveInput * Time.deltaTime * speed;
+        transform.position += delta;
     }
 
     void OnMove(InputValue value)
