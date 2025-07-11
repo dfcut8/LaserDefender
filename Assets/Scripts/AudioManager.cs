75b6188fc50bb202c384ce0a88ca0c95ca6c9544
dfcut8 <dfcut8@proton.me>
2025-07-11 10:23:08 -0400

    refactor audio manager to events

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -14,6 +14,8 @@ public class AudioManager : MonoBehaviour
     public AudioClip EnemyExplosion1;
     [Range(0.0f, 1.0f)] public float EnemyExplosion1Volume = 0.5f;
 
+    public EnemySpawner enemySpawner;
+
     private void Awake()
     {
         if (backgroundMusicClip != null)
@@ -27,6 +29,34 @@ public class AudioManager : MonoBehaviour
         else
         {
             Debug.LogWarning("Background music clip is not assigned in AudioManager.");
+        }
+    }
+
+    void OnEnable()
+    {
+        if (enemySpawner != null)
+        {
+            enemySpawner.OnEnemySpawn.AddListener(HandleEnemySpawned);
+        }
+    }
+
+    private void HandleEnemySpawned(GameObject enemy)
+    {
+        //if (enemy.TryGetComponent<Shooter>(out var shooter))
+        //{
+        //    shooter.OnShoot.AddListener(PlayShootingClip);
+        //}
+        //else
+        //{
+        //    Debug.LogWarning("Enemy spawned does not have a Shooter component.");
+        //}
+        if (enemy.TryGetComponent<HealthManager>(out var healthManager))
+        {
+            healthManager.OnHit.AddListener(PlayEnemyExplosion1);
+        }
+        else
+        {
+            Debug.LogWarning("Enemy spawned does not have a HealthManager component.");
         }
     }
 
@@ -44,6 +74,7 @@ public class AudioManager : MonoBehaviour
     {
         if (shootingClip != null)
         {
+            Debug.Log("Playing shooting sound");
             AudioSource.PlayClipAtPoint(shootingClip, Camera.main.transform.position, shootingVolume);
         }
         else
@@ -56,6 +87,7 @@ public class AudioManager : MonoBehaviour
     {
         if (EnemyExplosion1 != null)
         {
+            Debug.Log("Playing explosion1 sound");
             AudioSource.PlayClipAtPoint(EnemyExplosion1, Camera.main.transform.position, EnemyExplosion1Volume);
         }
         else
